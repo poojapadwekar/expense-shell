aa9e9cae8f62da628619395080af17debc78bdd4
sauja <pkpadwekar21@gmail.com>
2023-09-10 21:29:08 +0530

    Practice1

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 a=/tmp/expense.log
 
-download and extract(){
+download_and_extract() {
   curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >>$a
   echo unzipping
   unzip /tmp/$component >>$a
--- frontend.sh
+++ frontend.sh
@@ -2,14 +2,11 @@ source common.sh
 common=frontend
 echo Installing the nginx
 dnf install nginx -y >>$a
-
 Removing the congiguration
 rm -rf /usr/share/nginx/html/* >>$a
 cp expense.conf /etc/nginx/default.d/expense.conf >>$a
 cd /usr/share/nginx/html >>$a
-download and extarct
-
-
+download_and_extract
 echo starting the service
 systemctl enable nginx >>/tmp/expense.log
 systemctl restart nginx >>/tmp/expense.log
